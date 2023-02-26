95301ca219122b6ebac46abffd2e1f78111922a0
lelis <lelis685@gmail.com>
2023-02-26 18:05:46 -0300

    config strategy cooperative consumer

---
+++ kafka-basics/src/main/java/br/com/demos/kafka/ConsumerCooperativeDemo.java
@@ -0,0 +1,93 @@
+package br.com.demos.kafka;
+
+
+import br.com.demos.kafka.config.ClusterProperties;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+@SpringBootApplication
+public class ConsumerCooperativeDemo {
+
+    private static ClusterProperties clusterProperties;
+
+
+    @Autowired
+    public void setClusterProperties(ClusterProperties clusterProperties) {
+        ConsumerCooperativeDemo.clusterProperties = clusterProperties;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerCooperativeDemo.class, args);
+
+
+        String groupId = "my-java-app";
+        String topic = "demo_java";
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", clusterProperties.getBootstrapServers());
+        properties.setProperty("security.protocol", clusterProperties.getSecurityProtocol());
+        properties.setProperty("sasl.jaas.config", clusterProperties.getSaslJaasConfig());
+        properties.setProperty("sasl.mechanism", clusterProperties.getSaslMechanism());
+
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        final Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Detected a shutdown, calling consumer.wakeup()");
+                consumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+
+        try {
+            consumer.subscribe(List.of(topic));
+
+            while (true) {
+
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+                for (var record : records) {
+                    System.out.println("Key: " + record.key());
+                    System.out.println("value: " + record.value());
+                    System.out.println("partition: " + record.partition());
+                    System.out.println("offset: " + record.offset());
+                }
+            }
+        }catch (WakeupException e){
+            System.out.println("consumer starting shut down");
+        }catch (Exception e){
+            System.out.println("Uncaught Error " + e );
+        }finally {
+            consumer.close();
+            System.out.println("consumer is shut down");
+        }
+
+
+    }
+
+}
